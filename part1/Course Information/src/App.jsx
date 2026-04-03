cbf1b13445e7708d639a23cf11c2ac70f382dbbd
SIDDHARTHA <death_baker895@deathCollecter6.localdomain>
2026-04-03 11:33:46 +0200

    solved part 1.1-1.2

---
+++ part1/Course Information/src/App.jsx
@@ -0,0 +1,56 @@
+const Header = (param) =>{
+  return (
+    <h1>{param.course}</h1>
+  )
+}
+const Part = (param) => {
+  return (
+    <p>
+      {param.part} {param.exercise}
+    </p>
+  )
+}
+ const Content = (param) =>{
+  return(
+      <div>
+        <Part part = {param.part1} exercise = {param.exercises1} />
+        <Part part = {param.part2} exercise = {param.exercises2} />
+        <Part part = {param.part3} exercise = {param.exercises3} />
+      </div>
+  )
+ }
+
+const Total = (param) =>{
+  return (
+    <p>
+     Number of exercises {param.exercise1+param.exercise2+param.exercise3}
+    </p>
+  )
+}
+
+
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <div>
+      <Header course = {course} />
+      <Content part1 = {part1} exercises1={exercises1}
+       part2 = {part2} exercises2= {exercises2} 
+       part3 = {part3} exercises3 = {exercises3} />
+      <Total exercise1={exercises1}
+       exercise2 = {exercises2}
+       exercise3 = {exercises3} />
+    </div>
+  )
+}
+
+export default App
